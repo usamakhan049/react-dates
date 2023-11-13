25e800dd638f2936aac9e978f8c458c3098bc70a
Usama Khan <>
2023-11-13 20:51:23 +0500

    change day size

--- src/constants.js
+++ src/constants.js
@@ -26,7 +26,7 @@ export const ANCHOR_RIGHT = 'right';
 export const OPEN_DOWN = 'down';
 export const OPEN_UP = 'up';
 
-export const DAY_SIZE = 39;
+export const DAY_SIZE = 30;
 export const BLOCKED_MODIFIER = 'blocked';
 export const WEEKDAYS = [0, 1, 2, 3, 4, 5, 6];
 
